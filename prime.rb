c55bb248238825db897d63e5f1626f14ec9f759a
Junyoung McArdle <juny.mcardle@gmail.com>
2019-03-21 23:16:04 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,27 +1,12 @@
 require 'pry'
-# def prime?(num)
-#   n = 2
-#   if num <= 1
-#     return false
-#   end
-#   while n < num
-#     return false if num % n == 0
-#       n += 1
-#   end
-#     true
-# end
-
 def prime?(num)
   n = 2
-  bool = nil
-    if num <= 1
-      return false
-    end
-  while n < num
-    if num % n == 0
-     return bool = false
-     n += 1
-    end
+  if num <= 1
+    return false
   end
-  return bool
+  while n < num
+    return false if num % n == 0
+      n += 1
+  end
+    true
 end
