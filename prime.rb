2f290ffa32133c65cddadfb853721aa9b82ce5c8
Junyoung McArdle <juny.mcardle@gmail.com>
2019-03-21 23:10:59 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,12 +1,26 @@
 require 'pry'
-def prime?(num)
+# def prime?(num)
+#   n = 2
+#   if num <= 1
+#     return false
+#   end
+#   while n < num
+#     return false if num % n == 0
+#       n += 1
+#   end
+#     true
+# end
+
+def is_prime(num)
   n = 2
-  if num <= 1
-    return false
-  end
+    if num <= 1
+      return false
+    end
   while n < num
-    return false if num % n == 0
-      n += 1
+    if num % n == 0
+     return false 
+     n += 1
+    end
+  true
   end
-    true
 end
